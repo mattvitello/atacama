3b2cc7b17f44deb4addd5acab163cd204c22c929
mattvitello <matt.vitello@cosmos.space>
2023-11-06 18:33:46 -0500

    remove canopy-perspectives references

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'canopy-perspectives'`, () => {
+  it(`should have as title 'atacama-manufacturing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('canopy-perspectives');
+    expect(app.title).toEqual('atacama-manufacturing');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('canopy-perspectives app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('atacama-manufacturing app is running!');
   });
 });
